890c0c7c8e888a0714a2894b86de34c6d9605b41
unknown <mazenosama987654321@gmail.com>
2026-03-28 22:18:56 +0200

    sale 3la rsol allah

---
+++ back/hospitalback/hospitalback/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hospitalback project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospitalback.settings')
+
+application = get_asgi_application()
